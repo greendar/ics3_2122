35b836cd9d107ae8cf648d20f341b51448df04e0
Mr Green <dwgreen@hwdsb.on.ca>
2022-02-28 15:05:31 -0500

    Create proj1Example.py

---
+++ proj1Example.py
@@ -0,0 +1,9 @@
+class Character:
+    def __init__(self, name, prof, level):
+        self.name = name
+        self.prof = prof
+        self.level = int(level)
+        
+while True:
+    charSheet = []
+    
