2f3fdc7fe44692d5d300e0bcbc6795bc4c46e313
Mr Green <dwgreen@hwdsb.on.ca>
2022-03-09 14:12:21 -0500

    Create workingWithFiles.py

---
+++ workingWithFiles.py
@@ -0,0 +1,33 @@
+"""
+
+fileName = 'pleaseWork.txt'
+
+f = open(fileName, 'r')
+
+for barney in f:
+    print(barney, end='')
+"""
+
+"""
+name = input("What should I name the file?:   ")
+
+name += ".txt"
+
+f = open(name, 'a')
+
+while True:
+    stuff = input("Enter Stuff:  ")
+    if stuff == 'end':
+        break
+    f.write(stuff)
+    f.write('\n')
+    
+f.close()
+print('Program Done')
+
+"""
+
+
+
+
+
